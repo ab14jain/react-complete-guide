887f6b74d96799c4311e3628456d4a9f2b563390
Abhishek <ab14jain@gmail.com>
2018-06-12 01:02:24 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,17 +6,31 @@ class App extends Component {
   state = {
     persons:[
       { name: "Abhishek", age: 28 },
-      { name: "Adarsh", age: 26 }
+      { name: "Adarsh", age: 26 },
+      { name: "Sumit", age: 27},
+      { name: "Rahul", age: 30}
     ]
+  }
+
+  nameSwitchHandler = () => {
+    this.setState({
+      persons:[
+        { name: "Abhishek!!!!", age: 28 },
+        { name: "Adarsh", age: 26 },
+        { name: "Sumit", age: 27},
+        { name: "Rahul", age: 30}
+      ]
+    })
   }
   render() {
     return (
       <div className="App">
         <h1>React App</h1> 
-        <Person name = "Abhishek" age="28"/> 
-        <Person name = "Bashir" age="22"/>
-        <Person name = "Satya" age="21">My hobby is Boxing</Person>
-        <Person name = "Adarsh" age="21"/>      
+        <button onClick={this.nameSwitchHandler}>Switch Name</button>
+        <Person name = {this.state.persons[0].name} age= {this.state.persons[0].age}/> 
+        <Person name = {this.state.persons[1].name} age= {this.state.persons[1].age}/>
+        <Person name = {this.state.persons[2].name} age= {this.state.persons[2].age}>My hobby is Boxing</Person>
+        <Person name = {this.state.persons[3].name} age= {this.state.persons[3].age}/>      
       </div>      
     );
     // return React.createElement("div", {className: "App", id:"kieue2"}, "This is React")
